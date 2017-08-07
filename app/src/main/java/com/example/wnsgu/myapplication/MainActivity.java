48171e1e35bb4653cb5dac2980cfecf9c98e83f4
DESKTOP-3IU3EIB\wnsgu <wnsgud0750@gmail.com>
2017-08-08 01:00:07 +0900

    test2

--- app/src/main/java/com/example/wnsgu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wnsgu/myapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); 
+        setContentView(R.layout.activity_main);
         // Add code to print out the key hash
         try {
             PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
